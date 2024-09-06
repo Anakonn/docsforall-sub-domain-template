b51375191888d2a3d970ffec699c2c1143c2b587
ego <mrego@anakonn.com>
2024-09-06 17:44:36 +0900

    config

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,7 @@
 export const siteConfig = {
-	title: 'Sub Template',
-	url: 'https://docsforall.com/',
-	base: '/sub-template',
+	title: 'Sub Domain',
+	url: 'https://sub.docsforall.com/',
 	googleAdsSrc:
 		'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2130210715518535',
-	githubUrl: 'https://github.com/Anakonn/docsforall-sub-template',
+	githubUrl: 'https://github.com/Anakonn/docsforall-sub-domain-template',
 };
